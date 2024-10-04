0e875610c58106b0b1f8e8e4751a0440db1df8b5
tfuok <mr.thienphuocfat@gmail.com>
2024-10-04 19:22:29 +0700

    Full CRUD Breed

--- demo/src/main/java/com/example/demo/api/BreedAPI.java
+++ demo/src/main/java/com/example/demo/api/BreedAPI.java
@@ -1,6 +1,7 @@
 package com.example.demo.api;
 
 import com.example.demo.entity.Breed;
+import com.example.demo.model.Request.BreedRequest;
 import com.example.demo.service.BreedService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +19,18 @@ public class BreedAPI {
     BreedService breedService;
 
     @PostMapping
-    public ResponseEntity create(@RequestParam String breedName){
-        Breed breed = breedService.addNewBreed(breedName);
+    public ResponseEntity create(BreedRequest breedRequest){
+        Breed breed = breedService.addNewBreed(breedRequest);
         return ResponseEntity.ok(breed);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@PathVariable long id, @RequestParam String name){
+    public ResponseEntity update(@PathVariable long id, String name){
         Breed breed = breedService.changeBreedName(id, name);
         return ResponseEntity.ok(breed);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable long id){
         Breed breed = breedService.deleteBreed( id);
         return ResponseEntity.ok(breed);
