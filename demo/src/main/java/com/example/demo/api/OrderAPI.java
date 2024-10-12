5c6b86fa6dfbec51ad97283ae7ec347d8c94ae6d
tfuok <mr.thienphuocfat@gmail.com>
2024-10-12 23:34:27 +0700

    create new order

---
+++ demo/src/main/java/com/example/demo/api/OrderAPI.java
@@ -0,0 +1,40 @@
+package com.example.demo.api;
+
+import com.example.demo.entity.Account;
+import com.example.demo.entity.Orders;
+import com.example.demo.model.Request.OrderRequest;
+import com.example.demo.repository.OrderRepository;
+import com.example.demo.service.AuthenticationService;
+import com.example.demo.service.OrderService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/order")
+@CrossOrigin("*")//cho phép tất cả truy cập
+@SecurityRequirement(name = "api")
+public class OrderAPI {
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepo;
+
+    @Autowired
+    AuthenticationService authenticationService;
+    @PostMapping
+    public ResponseEntity create(@RequestBody OrderRequest orderRequest) throws Exception {
+        String url = orderService.createUrl(orderRequest);
+        return ResponseEntity.ok(url);
+    }
+
+    @GetMapping
+    public ResponseEntity get(){
+        Account account = authenticationService.getCurrentAccount();
+        List<Orders> orders = orderRepo.findOrderssByCustomer(account);
+        return ResponseEntity.ok(orders);
+    }
+}
